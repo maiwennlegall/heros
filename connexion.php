f3988a86aa39d4fc489406e6d470e960056da5bc
maiwennlegall <101551108+maiwennlegall@users.noreply.github.com>
2022-04-15 16:18:06 +0200

    page connexion

---
+++ connexion.php
@@ -0,0 +1,9 @@
+
+<h1>Connexion</h1>
+
+<div class="formulaire">
+        <form method="post" action="traitement.php"> <!--changer le fichier dans action-->
+        <input type="text"  name="login" placeholder="Entrez votre login" > <br/><br/>
+        <input type="password"  name="mdp" placeholder="Entrez votre mot de passe" ><br/><br/>
+        <input type="submit" name="se_connecter" id="se_connecter" value="Se connecter"/>            
+</div>
