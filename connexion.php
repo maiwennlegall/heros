381bbd44d9d62d0dc25594353c3572bcea6fa560
maiwennlegall <101551108+maiwennlegall@users.noreply.github.com>
2022-04-22 11:02:38 +0200

    connexion
    php de la page de connexion

--- connexion.php
+++ connexion.php
@@ -1,4 +1,23 @@
-<?php require_once "includes/head.php"; ?>
+<?php require_once "includes/head.php";
+require_once "includes/functions.php";
+include("includes/connect.php"); 
+session_start();
+
+if (!empty($_POST['login']) and !empty($_POST['mdp'])) {
+    $login = $_POST['login'];
+    $password = $_POST['mdp'];
+    $stmt = $BDD->prepare('select * from utilisateur where identifiant=? and mdp=?');
+    $stmt->execute(array($login, $password));
+    if ($stmt->rowCount() == 1) {
+        // Authentication successful
+        $_SESSION['login'] = $login;
+        redirect("accueil.php");
+    }
+    else {
+        $error = "Utilisateur non reconnu";
+    }
+}
+?>
 
 <body>
     <div class="container">
@@ -7,9 +26,13 @@
     </div>
         <br/> <br/><br/>
     <h1>Connexion</h1>
-
+<?php if (isset($error)) { ?>
+        <div class="alert alert-danger">
+        <strong>Erreur !</strong> <?= $error ?>
+        </div>
+<?php } ?>
 <div class="formulaire">
-        <form method="post" action="traitement.php"> <!--changer le fichier dans action-->
+        <form method="post" action="connexion.php"> <!--changer le fichier dans action-->
         <input type="text"  name="login" placeholder="Entrez votre login" > <br/><br/>
         <input type="password"  name="mdp" placeholder="Entrez votre mot de passe" ><br/><br/>
         <input type="submit" name="se_connecter" id="se_connecter" value="Se connecter"/>            
--- includes/connect.php
+++ includes/connect.php
@@ -1,12 +1,19 @@
 <?php
+    $host = "localhost";
+    $dbname = "storyofyourlife";
+    $username = "storyofyourlife";
+    $password = "herooflife";
+
     try 
     {
-        $BDD = new PDO( "mysql:host=localhost;dbname=storyofyourlife;charset=utf8",
-        "storyofyourlife","herooflife", array(PDO::ATTR_ERRMODE
-        =>PDO::ERRMODE_EXCEPTION));
+        $BDD = new PDO( "mysql:host=".$host.";dbname=".$dbname.";charset=utf8",
+        $username,$password);
     }
     catch (Exception $e) 
     {
         die('Erreur fatale : ' . $e->getMessage());
     }
+
+#,array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION)
 ?>
+
