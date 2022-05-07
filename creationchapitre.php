0bae9fe58d4275e4962ee54818dade4a8df58f9a
oledur <101526516+oledur@users.noreply.github.com>
2022-05-07 22:46:18 +0200

    correctif bug chapitre

--- creationchapitre.php
+++ creationchapitre.php
@@ -1,4 +1,3 @@
-<?php //session_start() ?>
 <?php include("includes/connect.php"); ?>
 <?php
 require_once "includes/functions.php";
@@ -23,17 +22,26 @@ require_once "includes/functions.php";
         $data = $res->fetch();                  
         $nb_chapitres_faits = $data['nb'];
 
-        $res = $BDD->query('SELECT count(id_ch_choix1) as nbr from chapitre WHERE id_ch_choix1!="NULL"');
+        /*$res = $BDD->query('SELECT count(id_ch_choix1) as nbr from chapitre WHERE id_ch_choix1!="NULL"');
         $data = $res->fetch();                 
-        $nb_chapitres_pas_fin = $data['nbr'];
+        $nb_chapitres_pas_fin = $data['nbr'];*/
                 
         if(isset($_POST['fin']))
         {
-            $maReq = $BDD -> prepare("INSERT INTO chapitre (id_chapitre, titre, id_hist, textes) VALUES (:id, :title, :hist, :ecriture)");
+            if($_POST['fin']=="Fin_positive")
+            {
+                $fin=0;
+            }
+            else
+            {
+                $fin=1;
+            }
+            $maReq = $BDD -> prepare("INSERT INTO chapitre (id_chapitre, titre, id_hist, type_fin, textes) VALUES (:id, :title, :hist, :fin, :ecriture)");
             $maReq -> execute(array(
                     'id' => $nb_chapitres_faits+1,
                     'title' => $_POST['titre'],
                     'hist' => $_GET['histoire'],
+                    'fin' => $fin,
                     'ecriture' => $_POST["resumer"],
                 ));
         }
@@ -41,7 +49,7 @@ require_once "includes/functions.php";
         {
             if(empty($_POST['ch1'])||empty($_POST['ch2'])||empty($_POST['ch3']))
             {
-                $error = "Vous n'avez pas tout rempli !";
+                //$error = "Vous n'avez pas tout rempli !";
                 redirect("creationchapitre.php?histoire=".$_GET['histoire']."&debut=".$_GET['debut']);
             }
             else
@@ -52,16 +60,15 @@ require_once "includes/functions.php";
                             'title' => $_POST['titre'],
                             'hist' => $_GET['histoire'],
                             'vie' => $_POST['vie'],
-                            'id1' => ($nb_chapitres_pas_fin*3)+2,
-                            'id2' => ($nb_chapitres_pas_fin*3)+3,
-                            'id3' => ($nb_chapitres_pas_fin*3)+4,
+                            'id1' => $nb_chapitres_faits+2,
+                            'id2' => $nb_chapitres_faits+3,
+                            'id3' => $nb_chapitres_faits+4,
                             't1' => $_POST["ch1"],
                             't2' => $_POST['ch2'],
                             't3' => $_POST["ch3"],
 
                             'ecriture' => $_POST["resumer"],
-                            ));
-                        
+                            ));    
             }
         }
     }
@@ -143,31 +150,6 @@ require_once "includes/functions.php";
 
             <label for="ch3">Choix n°3</label>
             <input type="text"  name="ch3"> 
-<!--
-            <label for="title">Titre de votre chapitre</label>
-            <input type="text" name="titre"> <br/><br/>
-
-            <textarea name="resumer" cols="50" rows="7">Texte de votre chapitre</textarea> <br/><br/>
-            
-
-            <label for="ch1">Choix n°1</label>
-            <input type="text"  name="ch1"> <br/><br/>
-
-            <label for="ch2">Choix n°2</label>
-            <input type="text"  name="ch2"> <br/><br/>
-
-            <label for="ch3">Choix n°3</label>
-            <input type="text"  name="ch3"> <br/><br/>
-
-            <label for="vie">Modification du nombre de vie (+1, -1...)</label>
-            <input type="number"  name="vie"> <br/><br/>
-
-            <label for="fin"> Cochez si cela constitue une fin </label>
-            <input type="radio" name="fin"> </br>
-
-            <button type="submit" class="btn btn-default btn-primary"> Chapitre fait ! </button>  -->
-
-            
             <fieldset>
                 <legend>Cochez si cela constitue une fin</legend>
 
